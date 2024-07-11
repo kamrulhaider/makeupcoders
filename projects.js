2fee6e4b1392948e1136da91198dcf05e24317ea
kamrulhaider <kamrulhaider.ssb@gmail.com>
2024-07-11 23:04:32 +0600

    Projects array add

---
+++ projects.js
@@ -0,0 +1,48 @@
+import images from "./public/images/images";
+
+const projectList = [
+  {
+    title: "Visamind",
+    des: "A visa info and visa application website.",
+    image: images.visamind,
+    category: "web",
+  },
+  {
+    title: "Adsusu",
+    des: "An micro investment website for the people who has lower saving.",
+    image: images.adsusu,
+    category: "web",
+  },
+  {
+    title: "Travelion",
+    des: "Air ticketing and hotel reservation website B2B.",
+    image: images.travelion,
+    category: "web",
+  },
+  {
+    title: "Retailer Info",
+    des: "A retailer management android app for the dealer to manage his transaction.",
+    image: images.retailerInfo,
+    category: "app",
+  },
+  {
+    title: "Turf Management",
+    des: "Football & Cricket Turf management app for managing their slots.",
+    image: images.turfmngmnt,
+    category: "app",
+  },
+  {
+    title: "E-Commerce",
+    des: "A single vendor e-commerce website UI.",
+    image: images.ecommerceui,
+    category: "uiux",
+  },
+  {
+    title: "Hotel Reservation",
+    des: "Multiple hotel reservation mobile app UI.",
+    image: images.hotelui,
+    category: "uiux",
+  },
+];
+
+export default projectList;
