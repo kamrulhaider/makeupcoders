08a811a6c942c9eef13ad75ec35029a7a01db6e5
kamrulhaider <kamrulhaider.ssb@gmail.com>
2024-07-11 23:04:48 +0600

    teams array add

---
+++ teams.js
@@ -0,0 +1,48 @@
+const { default: images } = require("./public/images/images");
+
+const teamList = [
+  {
+    name: "Saran Barua",
+    position: "Co-founder & MERN Stack Developer",
+    image: images.saran,
+    linkedin: "https://www.linkedin.com/in/saran-barua/",
+    github: "https://github.com/saranbarua",
+  },
+  {
+    name: "Atul Debnath",
+    position: "Co-founder & Backend Developer",
+    image: images.atul,
+    linkedin: "https://www.linkedin.com/in/atul9t8/",
+    github: "https://github.com/atul9t8",
+  },
+  {
+    name: "Tanmoy Barua",
+    position: "Co-founder & Advisor",
+    image: images.tonmoy,
+    linkedin: "https://www.linkedin.com/in/tanmoybarua/",
+    github: "https://github.com/Tanmoy-Barua",
+  },
+  {
+    name: "Kamrul Haider Chowdhury",
+    position: "Co-founder & Frontend Developer",
+    image: images.kamrul,
+    linkedin: "https://www.linkedin.com/in/kamrulhaider/",
+    github: "https://github.com/kamrulhaider",
+  },
+  {
+    name: "Md Saimon Hossen",
+    position: "UI/UX Designer, Product Designer & Graphic Designer",
+    image: images.saimon,
+    linkedin: "https://www.linkedin.com/in/saimonhossen/",
+    behance: "https://www.behance.net/SaimonHossen",
+  },
+  {
+    name: "Prothom Acharjee",
+    position: "PHP & Laravel Developer",
+    image: images.prothom,
+    linkedin: "https://www.linkedin.com/in/prothom-acharjee/",
+    github: "https://github.com/prothomacharjee",
+  },
+];
+
+export default teamList;
