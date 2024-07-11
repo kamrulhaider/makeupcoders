d539cb33ef503cb41cd78c04c9868b43dc5ceb3a
kamrulhaider <kamrulhaider.ssb@gmail.com>
2024-07-11 23:05:42 +0600

    images add

---
+++ public/images/images.js
@@ -0,0 +1,65 @@
+import logo from "./logo/logo.png";
+import heroImg from "./hero/hero-img.png";
+import worldMap from "./hero/world-map.png";
+import webdev from "./services/web-development.jpg";
+import appdev from "./services/app-development.jpg";
+import grapicdesign from "./services/grapic-design.jpg";
+import friendly from "./howwe/friendly.svg";
+import passionate from "./howwe/passion.svg";
+import resourceful from "./howwe/resourcefull.svg";
+import facebook from "./social/facebook.png";
+import instagram from "./social/instagram.png";
+import linkedin from "./social/linkedin.png";
+import github from "./social/github.png";
+import ouroffice from "./about/ouroffice.jpg";
+import teamdetails from "./about/teamsection.jpg";
+import saran from "./about/saran.jpeg";
+import atul from "./about/atul.jpeg";
+import kamrul from "./about/kamrul.jpeg";
+import realdeal from "./about/realdeal.jpg";
+import visamind from "./projects/visamind.png";
+import adsusu from "./projects/adsusu.png";
+import travelion from "./projects/travilion.png";
+import saimon from "./about/saimon.jpeg";
+import behance from "./social/behance.png";
+import prothom from "./about/prothom.jpeg";
+import retailerInfo from "./projects/retailer-info.jpg";
+import turfmngmnt from "./projects/turf-mngment.jpg";
+import tonmoy from "./about/tanmoy.jpg";
+import ecommerceui from "./projects/ecommerce-ui.png";
+import hotelui from "./projects/hotel-ui.png";
+
+const images = {
+  logo,
+  heroImg,
+  worldMap,
+  webdev,
+  appdev,
+  grapicdesign,
+  friendly,
+  passionate,
+  resourceful,
+  facebook,
+  instagram,
+  linkedin,
+  github,
+  ouroffice,
+  teamdetails,
+  saran,
+  atul,
+  kamrul,
+  realdeal,
+  visamind,
+  adsusu,
+  travelion,
+  saimon,
+  behance,
+  prothom,
+  retailerInfo,
+  turfmngmnt,
+  tonmoy,
+  ecommerceui,
+  hotelui,
+};
+
+export default images;
