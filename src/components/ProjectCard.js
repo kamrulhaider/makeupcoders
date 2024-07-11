111c45d5e1c0ffef3fe6dd278e1d54a00b54b74d
kamrulhaider <kamrulhaider.ssb@gmail.com>
2024-07-11 23:07:57 +0600

    Project card add

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function ProjectCard({ image, title, des }) {
+  return (
+    <div className="text-center bg-blue-900 rounded-2xl hover:scale-[1.01] transition-all ease-in">
+      <Image src={image} className="rounded-t-2xl w-full" alt="" />
+      <h4 className="text-2xl uppercase mt-3">{title}</h4>
+      <p className="text-sm mb-3 px-4">{des}</p>
+    </div>
+  );
+}
