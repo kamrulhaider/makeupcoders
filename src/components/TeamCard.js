841fd47336866188b307c3ee30b8cccc8a39b350
kamrulhaider <kamrulhaider.ssb@gmail.com>
2024-07-11 23:08:25 +0600

    Team card add

---
+++ src/components/TeamCard.js
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import images from "../../public/images/images";
+
+export default function TeamCard({
+  name,
+  position,
+  image,
+  linkedin,
+  github,
+  behance,
+}) {
+  return (
+    <div className="p-10 text-center">
+      <Image src={image} className="w-full rounded-full" alt="Team Member" />
+      <h4 className="text-2xl mt-5">{name}</h4>
+      <p>{position}</p>
+      <div className="flex gap-3 justify-center mt-5">
+        <Link suppressHydrationWarning href={linkedin}>
+          <Image
+            className="h-8 w-full"
+            src={images.linkedin}
+            alt="LinkedIn icon"
+          />
+        </Link>
+        {github && (
+          <Link suppressHydrationWarning href={github}>
+            <Image
+              className="h-8 w-full"
+              src={images.github}
+              alt="LinkedIn icon"
+            />
+          </Link>
+        )}
+        {behance && (
+          <Link suppressHydrationWarning href={behance}>
+            <Image
+              className="h-8 w-full"
+              src={images.behance}
+              alt="LinkedIn icon"
+            />
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+}
